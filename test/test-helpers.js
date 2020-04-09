8f0a132f8effa73d0a054829633fdacfdfce01db
Jack Lansing <jack.lansing@live.com>
2020-04-09 12:06:01 -0400

    minor refactor to seedThingsTables

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -269,12 +269,13 @@ function seedThingsTables(db, users, things = [], reviews = []) {
       await trx.raw(`SELECT setval('thingful_things_id_seq', ?)`, [
         things[things.length - 1].id
       ]);
-    }
-    if (reviews.length) {
-      await trx.into('thingful_reviews').insert(reviews);
-      await trx.raw(`SELECT setval('thingful_reviews_id_seq', ?)`, [
-        reviews[reviews.length - 1].id
-      ]);
+
+      if (reviews.length) {
+        await trx.into('thingful_reviews').insert(reviews);
+        await trx.raw(`SELECT setval('thingful_reviews_id_seq', ?)`, [
+          reviews[reviews.length - 1].id
+        ]);
+      }
     }
   });
 }
